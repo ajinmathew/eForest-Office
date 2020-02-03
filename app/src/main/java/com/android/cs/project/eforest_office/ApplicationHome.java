3e6d1895987e937ec8cde437aee86fdc42f0aae0
ajinmathew <mathewajin10@gmail.com>
2020-02-03 07:54:42 +0530

    home page and google map implemented

---
+++ app/src/main/java/com/android/cs/project/eforest_office/ApplicationHome.java
@@ -0,0 +1,14 @@
+package com.android.cs.project.eforest_office;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApplicationHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_home);
+    }
+}
